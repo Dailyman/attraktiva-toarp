95085969815e797f39754ff6cd4935e92fb0150b
eriqa <eriqa@hotmail.com>
2014-11-06 16:05:07 +0000

    Dummy-file added.

--- EventHandlingSystem/EventHandlingSystem/EventHandlingDataModel.Designer.cs
+++ EventHandlingSystem/EventHandlingSystem/EventHandlingDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Robin\Documents\EventHandSys\EventHandlingSystem\EventHandlingSystem\EventHandlingDataModel.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\Erica\Documents\EC Utbildning\19 Attraktiva Toarp\EventHandlingSystem\EventHandlingSystem\EventHandlingDataModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
---
+++ EventHandlingSystem/EventHandlingSystem/SQLDummy/AddDummyData.sql
@@ -0,0 +1,49 @@
+﻿USE [EventHandlingSystem]
+GO
+
+INSERT INTO [dbo].[Taxonomies]([Name],[Created],[IsDeleted])
+     VALUES('Publiceringstaxonomi','2014-11-06','0')           
+GO
+INSERT INTO [dbo].[Taxonomies]([Name],[Created],[IsDeleted])
+     VALUES('Kategoriseringstaxonomi','2014-11-06','0')           
+GO
+INSERT INTO [dbo].[Taxonomies]([Name],[Created],[IsDeleted])
+     VALUES('Anpassadkategoriseringstaxonomi','2014-11-06','0')           
+GO
+
+
+INSERT INTO [dbo].[TermSets]([Name],[TaxonomyId],[ParentTermSetId],[Created],[IsDeleted])
+     VALUES
+           ('Äspered',1,null,'2014-11-06','0') 		   
+GO
+
+
+INSERT INTO [dbo].[Terms]([Name],[Created],[IsDeleted])
+     VALUES
+           ('Äspered','2014-11-06','0')
+GO
+
+INSERT INTO [dbo].[TermsInTermSets]([Term_Id],[TermSet_Id])
+     VALUES
+           (1,1)
+GO
+
+
+INSERT INTO [dbo].[Events]([Title],[Description],[Summary],[Other],[Location],[ImageUrl],[DayEvent],[StartDate],[EndDate],
+[TargetGroup],[ApproximateAttendees],[AssociationId],[Created],[CreatedBy],[IsDeleted])
+     VALUES
+           ('Introfest till A.T. Success!','Introduktion, stor fest', 'Nice','','Löwaskog','http://www.photolakedistrict.co.uk/wp-content/uploads/events-FIREWORKS.jpg', '1','2014-11-03','2014-11-04',
+		   'Kompisar','15',1,'2014-11-01','Robin','0')
+GO
+
+
+INSERT INTO [dbo].[Calendars]([ViewMode],[Mode],[Created],[IsDeleted])
+     VALUES
+           ('Table', 'M', '2014-10-27', '0')
+GO
+
+
+INSERT INTO [dbo].[WebPages]([AssociationId],[ContentId],[LogoUrl],[Created],[IsDeleted],[Calendar_Id])
+     VALUES
+           (1,1,'http://upload.wikimedia.org/wikipedia/commons/6/6f/Bor%C3%A5s_municipal_arms.svg','2014-10-27', '0',1)
+GO
