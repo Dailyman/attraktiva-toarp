d96d9175f722c50d4c40fbc473a6e49dde9dfd46
eriqa <eriqa@hotmail.com>
2014-11-07 17:54:44 +0000

    AddDummyData.sql AddDummyTaxonomy.sql

--- EventHandlingSystem/EventHandlingSystem/SQLDummy/AddDummyData.sql
+++ EventHandlingSystem/EventHandlingSystem/SQLDummy/AddDummyData.sql
@@ -1,33 +1,6 @@
 ﻿USE [EventHandlingSystem]
 GO
 
-INSERT INTO [dbo].[Taxonomies]([Name],[Created],[IsDeleted])
-     VALUES('Publiceringstaxonomi','2014-11-06','0')           
-GO
-INSERT INTO [dbo].[Taxonomies]([Name],[Created],[IsDeleted])
-     VALUES('Kategoriseringstaxonomi','2014-11-06','0')           
-GO
-INSERT INTO [dbo].[Taxonomies]([Name],[Created],[IsDeleted])
-     VALUES('Anpassadkategoriseringstaxonomi','2014-11-06','0')           
-GO
-
-
-INSERT INTO [dbo].[TermSets]([Name],[TaxonomyId],[ParentTermSetId],[Created],[IsDeleted])
-     VALUES
-           ('Äspered',1,null,'2014-11-06','0') 		   
-GO
-
-
-INSERT INTO [dbo].[Terms]([Name],[Created],[IsDeleted])
-     VALUES
-           ('Äspered','2014-11-06','0')
-GO
-
-INSERT INTO [dbo].[TermsInTermSets]([Term_Id],[TermSet_Id])
-     VALUES
-           (1,1)
-GO
-
 
 INSERT INTO [dbo].[Events]([Title],[Description],[Summary],[Other],[Location],[ImageUrl],[DayEvent],[StartDate],[EndDate],
 [TargetGroup],[ApproximateAttendees],[AssociationId],[Created],[CreatedBy],[IsDeleted])
---
+++ EventHandlingSystem/EventHandlingSystem/SQLDummy/AddDummyTaxonomy.sql
@@ -0,0 +1,30 @@
+﻿USE [EventHandlingSystem]
+GO
+
+
+INSERT INTO [dbo].[Taxonomies]([Name],[Created],[IsDeleted])
+     VALUES('Publiceringstaxonomi','2014-11-06','0')           
+GO
+INSERT INTO [dbo].[Taxonomies]([Name],[Created],[IsDeleted])
+     VALUES('Kategoriseringstaxonomi','2014-11-06','0')           
+GO
+INSERT INTO [dbo].[Taxonomies]([Name],[Created],[IsDeleted])
+     VALUES('Anpassadkategoriseringstaxonomi','2014-11-06','0')           
+GO
+
+
+INSERT INTO [dbo].[TermSets]([Name],[TaxonomyId],[ParentTermSetId],[Created],[IsDeleted])
+     VALUES
+           ('Äspered',1,null,'2014-11-06','0') 		   
+GO
+
+
+INSERT INTO [dbo].[Terms]([Name],[Created],[IsDeleted])
+     VALUES
+           ('Äspered','2014-11-06','0')
+GO
+
+INSERT INTO [dbo].[TermsInTermSets]([Term_Id],[TermSet_Id])
+     VALUES
+           (1,1)
+GO
