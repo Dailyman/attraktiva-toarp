9cdabb149dfb4e6645f10cff4705edc4ea5a09c1
robin.lars.hakansson <robin.lars.hakansson@f6505244-a8a6-daa9-ae32-7e69905a8ea2>
2014-11-07 17:37:03 +0000

    Created some methods to get events.

---
+++ EventHandlingSystem/EventHandlingSystem/Database/Database.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventHandlingSystem.Database
+{
+    public class Database
+    {
+        public static  readonly EventHandlingDataModelContainer Context = new EventHandlingDataModelContainer();
+    }
+}
