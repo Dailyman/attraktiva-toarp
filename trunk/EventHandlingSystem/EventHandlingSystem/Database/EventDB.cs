d6689094487e5932ea548c0e00a62b5f4699c3a2
robin.lars.hakansson <robin.lars.hakansson@f6505244-a8a6-daa9-ae32-7e69905a8ea2>
2014-11-07 17:37:03 +0000

    Created some methods to get events.

---
+++ trunk/EventHandlingSystem/EventHandlingSystem/Database/EventDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventHandlingSystem.Database
+{
+    public class EventDB
+    {
+        private  static  readonly  EventHandlingDataModelContainer  Context = new EventHandlingDataModelContainer();
+
+        private static IEnumerable<Event> GetAllNotDeletedEvents()
+        {
+            return Context.Events.Where(e => !e.IsDeleted);
+        }
+
+        public static List<Event> GetEventsBySpecifiedNumberOfPreviousMonthsFromToday(int nrOfMonths = 3)
+        {
+            DateTime defaulTimeSpan = DateTime.Now.AddMonths(-(nrOfMonths));
+            return GetAllNotDeletedEvents().Where(e => (e.StartDate > defaulTimeSpan)).ToList();
+        }
+        
+    }
+}
