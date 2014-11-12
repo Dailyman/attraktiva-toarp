f048641a5b710066e87cf5f0969d73a30497129b
robin.lars.hakansson <robin.lars.hakansson@f6505244-a8a6-daa9-ae32-7e69905a8ea2>
2014-11-12 19:38:55 +0000

    Improved the EventDetails.aspx.

--- EventHandlingSystem/EventHandlingSystem/EventDetails.aspx.designer.cs
+++ EventHandlingSystem/EventHandlingSystem/EventDetails.aspx.designer.cs
@@ -13,6 +13,24 @@ namespace EventHandlingSystem {
     public partial class EventDetails {
         
         /// <summary>
+        /// DropDownListEvents control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownListEvents;
+        
+        /// <summary>
+        /// BtnSearch control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnSearch;
+        
+        /// <summary>
         /// Main control.
         /// </summary>
         /// <remarks>
