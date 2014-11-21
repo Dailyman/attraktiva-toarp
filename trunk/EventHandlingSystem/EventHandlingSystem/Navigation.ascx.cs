6ebd2a5e7fe5aa9a30184fcc9adb9c11330e6bd6
robin.lars.hakansson <robin.lars.hakansson@f6505244-a8a6-daa9-ae32-7e69905a8ea2>
2014-11-21 14:45:14 +0000

    Improved TaxonomyControl.ascx.

--- trunk/EventHandlingSystem/EventHandlingSystem/Navigation.ascx.cs
+++ trunk/EventHandlingSystem/EventHandlingSystem/Navigation.ascx.cs
@@ -8,16 +8,13 @@ using EventHandlingSystem.Database;
 
 namespace EventHandlingSystem
 {
-    public partial class TaxonomyControl : System.Web.UI.UserControl
+    public partial class Navigation1 : System.Web.UI.UserControl
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            LabelDisplay.Text = "";
             if (!IsPostBack)
             {
-                AddNodesToTreeView(TreeViewTaxonomy, 1);
-                AddNodesToTreeView(TreeViewTaxonomy, 2);
-                AddNodesToTreeView(TreeViewTaxonomy, 3);
+                AddNodesToTreeView(TreeViewNavigation, 1);
             }
         }
 
@@ -32,15 +29,14 @@ namespace EventHandlingSystem
                 {
                     Text = tax.Name,
                     Value = tax.Id.ToString(),
-                    SelectAction = TreeNodeSelectAction.Expand,
-                    ImageUrl = "~/Images/Aux_folder_16x16.gif"
+                    SelectAction = TreeNodeSelectAction.Expand
                 };
                 //Lägger till HuvudNoden (ex. Publiceringstaxonomi).
                 treeView.Nodes.Add(taxNode);
 
                 //Hämtar all TermSets som ligger på den översta nivån i taxonomin
                 List<TermSet> parentTermSets = TermSetDB.GetAllParentTermSetsByTaxonomy(tax).OrderBy(ts => ts.Name).ToList();
-                
+
                 //Lägger till alla ParentNodes (ex. Äspered).
                 foreach (var parentTermSet in parentTermSets)
                 {
@@ -49,16 +45,13 @@ namespace EventHandlingSystem
                         Text = parentTermSet.Name,
                         Value = parentTermSet.Id.ToString(),
                         ShowCheckBox = true,
-                        SelectAction = TreeNodeSelectAction.Expand,
-                        ImageUrl = "~/Images/Aux_folder_16x16.gif"
+                        SelectAction = TreeNodeSelectAction.Expand
                     };
-
-                    FindTermNodesAndAddToTermSetNode(parentTermSet, node);
                     taxNode.ChildNodes.Add(node);
 
                     //För att hitta alla ChildNodes till den aktuella ParentNoden.
                     FindChildNodesAndAddToParentNode(parentTermSet, node);
-                    
+
                 }
             }
         }
@@ -73,62 +66,24 @@ namespace EventHandlingSystem
                     Text = ts.Name,
                     Value = ts.Id.ToString(),
                     ShowCheckBox = true,
-                    SelectAction = TreeNodeSelectAction.Expand,
-                    ImageUrl = "~/Images/Aux_folder_16x16.gif"
+                    SelectAction = TreeNodeSelectAction.Expand
                 };
 
-
-                FindTermNodesAndAddToTermSetNode(ts, childNode);
                 parentNode.ChildNodes.Add(childNode);
-                
 
                 //För att hitta alla ChildNodes till den aktuella ParentNoden. 
                 //Redundant anropning av metoden görs för att bygga upp hela "grenen".
                 FindChildNodesAndAddToParentNode(ts, childNode);
             }
-            
-            
+
+
         }
 
-        public void FindTermNodesAndAddToTermSetNode(TermSet tSet, TreeNode tNode)
+        protected void TreeViewNavigation_OnSelectedNodeChanged(object sender, EventArgs e)
         {
-            foreach (var term in TermDB.GetAllTermsByTermSet(tSet))
-            {
-                TreeNode termNode = new TreeNode
-                {
-                    Text = term.Name,
-                    Value = term.Id.ToString(),
-                    ShowCheckBox = true,
-                    SelectAction = TreeNodeSelectAction.Expand,
-                    ImageUrl = "~/Images/Tag_30x30.png"
-                };
-
-                tNode.ChildNodes.Add(termNode);
-            }
-        }
-
-        protected void TreeViewTaxonomy_OnSelectedNodeChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        protected void BtnDelete_OnClick(object sender, EventArgs e)
-        {
-            foreach (TreeNode node in TreeViewTaxonomy.CheckedNodes)
-            {
-                LabelDisplay.Text += node.Value + ", ";
-            }
-
-            if(!String.IsNullOrEmpty(LabelDisplay.Text))
-            {
-                LabelDisplay.Text += "has been deleted, or have they...?";
-            }
-            else
-            {
-                LabelDisplay.Text += "None has been deleted";
-            }
-            
 
         }
+
+        
     }
 }
