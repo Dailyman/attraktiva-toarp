63be5f5924769206a4d08109393f3e4079230ac5
robin.lars.hakansson <robin.lars.hakansson@f6505244-a8a6-daa9-ae32-7e69905a8ea2>
2014-11-24 09:22:48 +0000

    Added 2 new DB classes.

---
+++ trunk/EventHandlingSystem/EventHandlingSystem/Database/AssociationDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventHandlingSystem.Database
+{
+    public class AssociationDB
+    {
+        private static readonly EventHandlingDataModelContainer Context = Database.Context;
+
+        private static IEnumerable<Association> GetAllNotDeletedAssociations()
+        {
+            return Context.Associations.Where(a => !a.IsDeleted);
+        }
+
+        public static Association GetAssociationById(int id)
+        {
+            return GetAllNotDeletedAssociations().SingleOrDefault(a => a.Id.Equals(id));
+        }
+
+
+    }
+}
