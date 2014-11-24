4635d9baabf184b75b4771711d2e090fb5410ddc
robin.lars.hakansson <robin.lars.hakansson@f6505244-a8a6-daa9-ae32-7e69905a8ea2>
2014-11-24 09:22:48 +0000

    Added 2 new DB classes.

---
+++ EventHandlingSystem/EventHandlingSystem/Database/CommunityDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventHandlingSystem.Database
+{
+    public class CommunityDB
+    {
+        private static readonly EventHandlingDataModelContainer Context = Database.Context;
+
+        private static IEnumerable<Community> GetAllNotDeletedCommunities()
+        {
+            return Context.Communities.Where(c => !c.IsDeleted);
+        }
+
+        public static Community GetCommunityById(int id)
+        {
+            return GetAllNotDeletedCommunities().SingleOrDefault(c => c.Id.Equals(id));
+        }
+    }
+}
