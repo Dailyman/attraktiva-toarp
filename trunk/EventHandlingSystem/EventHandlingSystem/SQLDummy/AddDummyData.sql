fe927a722c466eeb0cefe2c291591792398f7538
eriqa@hotmail.com <eriqa@hotmail.com@f6505244-a8a6-daa9-ae32-7e69905a8ea2>
2014-11-26 10:24:37 +0000

    Dummydata

--- trunk/EventHandlingSystem/EventHandlingSystem/SQLDummy/AddDummyData.sql
+++ trunk/EventHandlingSystem/EventHandlingSystem/SQLDummy/AddDummyData.sql
@@ -167,19 +167,19 @@ INSERT INTO [dbo].[Events]([Title],[Description],[Summary],[Other],[Location],[I
 [TargetGroup],[ApproximateAttendees],[AssociationId],[Created],[CreatedBy],[LatestUpdate],[UpdatedBy],[IsDeleted])
      VALUES
            ('Introfest till A.T. Success!','Introduktion, stor fest', 'Nice','','Löwaskog','http://distilleryimage11.ak.instagram.com/c4f2ff1cdcfc11e2843f22000a9e0722_7.jpg', 'https://www.facebook.com/events/738311299567860/?ref=2&ref_dashboard_filter=upcoming', '1','2014-11-03','2014-11-04',
-		   'Kompisar','15',1,'2014-11-01','Robin','2014-11-10','Erica','0')
+		   'Kompisar','15',18,'2014-11-01','Robin','2014-11-10','Erica','0')
 GO
 INSERT INTO [dbo].[Events]([Title],[Description],[Summary],[Other],[Location],[ImageUrl],[LinkUrl],[DayEvent],[StartDate],[EndDate],
 [TargetGroup],[ApproximateAttendees],[AssociationId],[Created],[CreatedBy],[LatestUpdate],[UpdatedBy],[IsDeleted])
      VALUES
            ('Trollfest','Introduktion, stor fest', 'Nice','','Löwaskog','http://photos-e.ak.instagram.com/hphotos-ak-xpf1/10401605_572299706221780_1214301464_n.jpg', null,'1','2014-08-03','2014-08-04',
-		   'Kompisar','15',3,'2014-11-01','Robin','2014-11-10','Erica','0')
+		   'Kompisar','15',8,'2014-11-01','Robin','2014-11-10','Erica','0')
 GO
 INSERT INTO [dbo].[Events]([Title],[Description],[Summary],[Other],[Location],[ImageUrl],[LinkUrl],[DayEvent],[StartDate],[EndDate],
 [TargetGroup],[ApproximateAttendees],[AssociationId],[Created],[CreatedBy],[LatestUpdate],[UpdatedBy],[IsDeleted])
      VALUES
            ('Nyårsmiddag','Introduktion, stor fest', 'Nice','','Löwaskog','http://www.photolakedistrict.co.uk/wp-content/uploads/events-FIREWORKS.jpg', 'https://www.facebook.com/events/1558495814386669/?ref=88&unit_ref=popular_with_friends','1','2015-01-15','2015-01-16',
-		   'Kompisar','15',10,'2014-11-01','Robin','2014-11-10','Erica','0')
+		   'Kompisar','15',16,'2014-11-01','Robin','2014-11-10','Erica','0')
 GO
 INSERT INTO [dbo].[Events]([Title],[Description],[Summary],[Other],[Location],[ImageUrl],[LinkUrl],[DayEvent],[StartDate],[EndDate],
 [TargetGroup],[ApproximateAttendees],[AssociationId],[Created],[CreatedBy],[LatestUpdate],[UpdatedBy],[IsDeleted])
@@ -190,8 +190,8 @@ GO
 INSERT INTO [dbo].[Events]([Title],[Description],[Summary],[Other],[Location],[ImageUrl],[LinkUrl],[DayEvent],[StartDate],[EndDate],
 [TargetGroup],[ApproximateAttendees],[AssociationId],[Created],[CreatedBy],[LatestUpdate],[UpdatedBy],[IsDeleted])
      VALUES
-           ('Klappa hajar','Kom och bekanta dig med våra hajvänner', 'Lek med hajar','Friendly','Grannens pool','http://thumbs.dreamstime.com/x/cute-inflated-shark-5593365.jpg', 'https://www.facebook.com/TheCove?fref=pb&hc_location=profile_browser', '1','2014-12-24','2015-12-24',
-		   'Familj och vänner','15',11,'2014-11-26','Robin','2014-11-26','Erica','0')
+           ('Klappa hajar','Kom och bekanta dig med våra hajvänner', 'Lek med hajar','Friendly','Grannens pool','http://thumbs.dreamstime.com/x/cute-inflated-shark-5593365.jpg', 'https://www.facebook.com/TheCove?fref=pb&hc_location=profile_browser', '1','2014-12-24','2014-12-24',
+		   'Familj och vänner','15',19,'2014-11-26','Robin','2014-11-26','Erica','0')
 GO
 
 
