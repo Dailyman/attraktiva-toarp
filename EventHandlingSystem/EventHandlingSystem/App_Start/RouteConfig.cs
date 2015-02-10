821575e0b38bb836315a5ca33b4bb886bd9100fd
robin.lars.hakansson <robin.lars.hakansson@f6505244-a8a6-daa9-ae32-7e69905a8ea2>
2015-02-10 14:28:37 +0000

    Improved UserHandlingControl and some other stuff. Hopefully nothing will break ;_;

--- EventHandlingSystem/EventHandlingSystem/App_Start/RouteConfig.cs
+++ EventHandlingSystem/EventHandlingSystem/App_Start/RouteConfig.cs
@@ -10,7 +10,9 @@ namespace EventHandlingSystem
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.EnableFriendlyUrls();
+            var settings = new FriendlyUrlSettings();
+            settings.AutoRedirectMode = RedirectMode.Permanent;
+            routes.EnableFriendlyUrls(settings);
         }
     }
 }
