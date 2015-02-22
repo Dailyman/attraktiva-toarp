689ec5e59073e4f8122cce4b153b48a19fc88edf
robin.lars.hakansson <robin.lars.hakansson@f6505244-a8a6-daa9-ae32-7e69905a8ea2>
2015-02-22 17:06:23 +0000

    Added EventList.

---
+++ EventHandlingSystem/EventHandlingSystem/EventList.ascx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EventHandlingSystem.Database;
+
+namespace EventHandlingSystem
+{
+    public partial class EventList : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RenderEventList(new DateTime(2000,01,01), new DateTime(2050,01,01));
+        }
+
+        //DateTime eventdate = Convert.ToDateTime("03/15/2015");
+
+        public void RenderEventList(DateTime sDate, DateTime eDate)
+        {
+            List<events> eventList = EventDB.GetEventsByRangeDate(sDate, eDate);
+            
+            RepeaterEvents.DataSource = eventList;
+            RepeaterEvents.DataBind();
+
+        }
+
+        protected void BtnFilter_OnClick(object sender, EventArgs e)
+        {
+            RenderEventList(Convert.ToDateTime(TxtStart.Text), Convert.ToDateTime(TxtEnd.Text));
+        }
+    }
+}
