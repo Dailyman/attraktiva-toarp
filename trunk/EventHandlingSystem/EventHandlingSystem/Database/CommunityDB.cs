4cbc98d2fb6fea573cbc266d90793537dcc40d26
eriqa@hotmail.com <eriqa@hotmail.com@f6505244-a8a6-daa9-ae32-7e69905a8ea2>
2015-03-25 20:25:26 +0000

    Delete Communities and associations and web pages! Yay! \(^O^)/

--- trunk/EventHandlingSystem/EventHandlingSystem/Database/CommunityDB.cs
+++ trunk/EventHandlingSystem/EventHandlingSystem/Database/CommunityDB.cs
@@ -137,5 +137,56 @@ namespace EventHandlingSystem.Database
             }
             return true;
         }
+
+
+        // DELETE
+        public static int DeleteCommunityById(int id)
+        {
+            communities commToDelete = GetCommunityById(id);
+
+            if (commToDelete != null)
+                commToDelete.IsDeleted = true;
+
+            int affectedRows;
+
+            try
+            {
+                affectedRows = Context.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                foreach (DbEntityValidationResult item in ex.EntityValidationErrors)
+                {
+                    // Get entry
+                    DbEntityEntry entry = item.Entry;
+                    string entityTypeName = entry.Entity.GetType().Name;
+
+                    // Display or log error messages
+                    foreach (DbValidationError subItem in item.ValidationErrors)
+                    {
+                        string message = string.Format("Error '{0}' occurred in {1} at {2}",
+                                 subItem.ErrorMessage, entityTypeName, subItem.PropertyName);
+                        Console.WriteLine(message);
+                    }
+
+                    // Rollback changes
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            entry.State = EntityState.Detached;
+                            break;
+                        case EntityState.Modified:
+                            entry.CurrentValues.SetValues(entry.OriginalValues);
+                            entry.State = EntityState.Unchanged;
+                            break;
+                        case EntityState.Deleted:
+                            entry.State = EntityState.Unchanged;
+                            break;
+                    }
+                }
+                return affectedRows = 0;
+            }
+            return affectedRows;
+        }
     }
 }
