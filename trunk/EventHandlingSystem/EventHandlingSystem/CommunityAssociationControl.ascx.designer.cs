cac2760eb13f50afd183d4a66448079cd74fdac2
eriqa@hotmail.com <eriqa@hotmail.com@f6505244-a8a6-daa9-ae32-7e69905a8ea2>
2015-04-14 19:33:07 +0000

    Finished CommAsso Manager. ^^v

--- trunk/EventHandlingSystem/EventHandlingSystem/CommunityAssociationControl.ascx.designer.cs
+++ trunk/EventHandlingSystem/EventHandlingSystem/CommunityAssociationControl.ascx.designer.cs
@@ -229,6 +229,15 @@ namespace EventHandlingSystem {
         protected global::System.Web.UI.WebControls.Label LabelCommSave;
         
         /// <summary>
+        /// assoListboxView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl assoListboxView;
+        
+        /// <summary>
         /// LabelAssoInComm control.
         /// </summary>
         /// <remarks>
